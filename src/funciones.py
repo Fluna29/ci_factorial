63293d38c9002bdce3a2b002d3f958fbbb5c0e4f
Fluna29 <fluna293107@gmail.com>
2025-02-27 20:40:45 +0100

    Tarea Finalizada

--- src/funciones.py
+++ src/funciones.py
@@ -1,8 +1,19 @@
+"""
+Este módulo contiene la función factorial(n) que calcula el factorial de un número entero n.
+"""
 def factorial(n):
+
+    """
+    Esta función calcula el factorial de un número entero n.
+    Para ello, comprueba que sea un número entero y que sea positivo.
+    Y en el caso de ser 0, devuelve 1.
+    :param n:
+    :return:
+    """
     if not isinstance(n, int):
-        raise TypeError("Input must be an integer")
+        raise TypeError("Input debe de ser un Integer")
     if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers")
+        raise ValueError("No se puede hacer el factorial de un número negativo")
     if n == 0:
         return 1
 
